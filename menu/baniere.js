b929074a8250392f167d946373343a649f305fb6
alexis-da <alexis.dacruz40@gmail.com>
2025-02-01 17:54:00 +0100

    Upload of the project. Creation of the header of the pages.

---
+++ menu/baniere.js
@@ -0,0 +1,15 @@
+const nav = document.getElementById("nav");
+const header = document.getElementById("header")
+const home = document.getElementById("home")
+const links = document.querySelectorAll(".link")
+
+nav.addEventListener("click", function () {
+  nav.classList.toggle("active");
+  header.classList.toggle("active");
+  home.classList.toggle("active");
+  for (let index = 0; index < links.length; index++) {
+    const element = links[index];
+    element.classList.toggle("active")
+  }
+});
+
